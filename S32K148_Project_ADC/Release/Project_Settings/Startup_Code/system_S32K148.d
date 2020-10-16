31b5671053f363f7dcbf1a9851fba9431ed54a3e
venkat <venkat.chemmarthi@gmail.com>
2020-10-16 09:54:13 +0530

    Add a new project code

---
+++ S32K148_Project_ADC/Release/Project_Settings/Startup_Code/system_S32K148.d
@@ -0,0 +1,59 @@
+Project_Settings/Startup_Code/system_S32K148.o: \
+ ../Project_Settings/Startup_Code/system_S32K148.c \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/lib_c99.prefix \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/common.prefix \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/ewl_c_version.h \
+ C:/Users/Administrator/Desktop/HCL_Projects/Aerospace/S32K148_Project_ADC/include/device_registers.h \
+ C:/Users/Administrator/Desktop/HCL_Projects/Aerospace/S32K148_Project_ADC/include/s32_core_cm4.h \
+ C:/Users/Administrator/Desktop/HCL_Projects/Aerospace/S32K148_Project_ADC/include/S32K148.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/stdint.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/ansi_parms.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/ewlGlobals.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/os_enum.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/cstdint \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/limits_api.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/ewl_lib_ext1.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/wchar_t.h \
+ C:/Users/Administrator/Desktop/HCL_Projects/Aerospace/S32K148_Project_ADC/include/S32K148_features.h \
+ C:/Users/Administrator/Desktop/HCL_Projects/Aerospace/S32K148_Project_ADC/include/devassert.h \
+ C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/stdbool.h \
+ C:/Users/Administrator/Desktop/HCL_Projects/Aerospace/S32K148_Project_ADC/include/system_S32K148.h
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/lib_c99.prefix:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/common.prefix:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/ewl_c_version.h:
+
+C:/Users/Administrator/Desktop/HCL_Projects/Aerospace/S32K148_Project_ADC/include/device_registers.h:
+
+C:/Users/Administrator/Desktop/HCL_Projects/Aerospace/S32K148_Project_ADC/include/s32_core_cm4.h:
+
+C:/Users/Administrator/Desktop/HCL_Projects/Aerospace/S32K148_Project_ADC/include/S32K148.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/stdint.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/ansi_parms.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/ewlGlobals.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/os_enum.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/cstdint:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/limits_api.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/ewl_lib_ext1.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/wchar_t.h:
+
+C:/Users/Administrator/Desktop/HCL_Projects/Aerospace/S32K148_Project_ADC/include/S32K148_features.h:
+
+C:/Users/Administrator/Desktop/HCL_Projects/Aerospace/S32K148_Project_ADC/include/devassert.h:
+
+C:/NXP/S32DS_ARM_v2.2/S32DS/build_tools/gcc_v4.9/arm_ewl2/EWL_C/include/stdbool.h:
+
+C:/Users/Administrator/Desktop/HCL_Projects/Aerospace/S32K148_Project_ADC/include/system_S32K148.h:
